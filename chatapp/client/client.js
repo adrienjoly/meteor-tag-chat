611ffc469fd343ae25f8dbb856c9030914c32d4b
Adrien Joly <adrien@whyd.com>
2015-03-28 23:12:47 +0100

    store user id in messages instead of name

--- chatapp/client/client.js
+++ chatapp/client/client.js
@@ -15,23 +15,16 @@ Template.messages.helpers({
 Template.input.events = {
   'keydown input#message' : function (event) {
     if (event.which == 13) { // 13 is the enter key event
-      if (Meteor.user())
-        var name = Meteor.user().profile.name;
-      else
-        var name = 'Anonymous';
+      var uId = (Meteor.user() || {})._id;
       var message = document.getElementById('message');
-
-      if (message.value != '') {
+      if (uId && message.value != '') {
         Messages.insert({
-          name: name,
+          uId: uId,
           message: message.value,
           time: Date.now(),
         });
-
-        document.getElementById('message').value = '';
         message.value = '';
       }
     }
   }
-}
 };
